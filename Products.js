efd3fcd1f47cffc7235378a1a751d67d7e0842ca
jaivalp22 <jaival.patel@ucalgary.ca>
2024-03-10 23:23:10 -0600

    minor changes

--- Products.js
+++ Products.js
@@ -1,19 +1,19 @@
 let cart = [];
 
-function addToCart(productName, productPriceString) {
+function addToCart(ProductName, productPriceString) {
     const productPrice = +productPriceString.replace("Price: $", "");
-    cart[productName] = cart[productName] || { price: productPrice, quantity: 0 };
-    cart[productName].quantity += 1;
+    cart[ProductName] = cart[ProductName] || { price: productPrice, quantity: 0 };
+    cart[ProductName].quantity += 1;
 
-    alert(`${productName} has been added to the cart.`);
+    alert(`${ProductName} has been added to the cart.`);
 
     updateCartDisplay();
 }
 
-function removeFromCart(productName) {
-    cart[productName].quantity -= 1;
-    if (cart[productName].quantity === 0) {
-        delete cart[productName];
+function removeFromCart(ProductName) {
+    cart[ProductName].quantity -= 1;
+    if (cart[ProductName].quantity === 0) {
+        delete cart[ProductName];
     }
 
     updateCartDisplay();
@@ -28,18 +28,17 @@ function updateCartDisplay() {
     displayTitle.style.textAlign = "center";
     cartDisplay.appendChild(displayTitle);
 
-    for (let productName in cart) {
-        const product = cart[productName];
+    for (let ProductName in cart) {
+        const product = cart[ProductName];
 
         const cartItem = document.createElement("div");
         cartItem.classList.add("cart-item");
         cartItem.innerHTML = `
-            <p>${productName} - ${product.price} - ${product.quantity} <button class="remove-button">Remove</button></p>
+            <p>${ProductName} - ${product.price} - ${product.quantity} <button class="remove-button">Remove</button></p>
         `;
 
         const removeButton = cartItem.querySelector(".remove-button");
-        removeButton.addEventListener("click", () => removeFromCart(productName));
-
+        removeButton.addEventListener("click", () => removeFromCart(ProductName));
         cartDisplay.appendChild(cartItem);
     }
 }
@@ -48,9 +47,9 @@ const addToCartButtons = document.querySelectorAll(".product button");
 addToCartButtons.forEach((button) => {
     button.addEventListener("click", function () {
         const productCard = this.closest(".product");
-        const productName = productCard.querySelector("h1").innerText;
+        const ProductName = productCard.querySelector("h1").innerText;
         const productPriceString = productCard.querySelector("p").innerText;
-        addToCart(productName, productPriceString);
+        addToCart(ProductName, productPriceString);
     });
 });
 
