1989f65f774310dc1bed749ed3a2d4cacecdb929
jaivalp22 <jaival.patel@ucalgary.ca>
2024-03-10 23:46:58 -0600

    small changes made

--- Products.js
+++ Products.js
@@ -1,19 +1,18 @@
-let cart = [];
+let cart = {};
 
-function addToCart(ProductName, productPriceString) {
-    const productPrice = +productPriceString.replace("Price: $", "");
-    cart[ProductName] = cart[ProductName] || { price: productPrice, quantity: 0 };
-    cart[ProductName].quantity += 1;
+function addToCart(productName, productPriceString) {
+    cart[productName] = cart[productName] || { price: productPriceString, quantity: 0 };
+    cart[productName].quantity += 1;
 
-    alert(`${ProductName} has been added to the cart.`);
+    alert(`${productName} has been added to the cart!`);
 
     updateCartDisplay();
 }
 
-function removeFromCart(ProductName) {
-    cart[ProductName].quantity -= 1;
-    if (cart[ProductName].quantity === 0) {
-        delete cart[ProductName];
+function removeFromCart(productName) {
+    cart[productName].quantity -= 1;
+    if (cart[productName].quantity === 0) {
+        delete cart[productName];
     }
 
     updateCartDisplay();
@@ -28,17 +27,17 @@ function updateCartDisplay() {
     displayTitle.style.textAlign = "center";
     cartDisplay.appendChild(displayTitle);
 
-    for (let ProductName in cart) {
-        const product = cart[ProductName];
+    for (let productName in cart) {
+        const product = cart[productName];
 
         const cartItem = document.createElement("div");
         cartItem.classList.add("cart-item");
         cartItem.innerHTML = `
-            <p>${ProductName} - ${product.price} - ${product.quantity} <button class="remove-button">Remove</button></p>
+            <p>Name: ${productName} - ${product.price} - Quantity: ${product.quantity} <button class="remove-button">Remove</button></p>
         `;
 
         const removeButton = cartItem.querySelector(".remove-button");
-        removeButton.addEventListener("click", () => removeFromCart(ProductName));
+        removeButton.addEventListener("click", () => removeFromCart(productName));
         cartDisplay.appendChild(cartItem);
     }
 }
@@ -47,9 +46,9 @@ const addToCartButtons = document.querySelectorAll(".product button");
 addToCartButtons.forEach((button) => {
     button.addEventListener("click", function () {
         const productCard = this.closest(".product");
-        const ProductName = productCard.querySelector("h1").innerText;
+        const productName = productCard.querySelector("h1").innerText;
         const productPriceString = productCard.querySelector("p").innerText;
-        addToCart(ProductName, productPriceString);
+        addToCart(productName, productPriceString);
     });
 });
 
